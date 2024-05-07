fc1f5d8c2ec278f9aac557308a8d2e5e5d02f358
Dinesh Kumar E <egdedk@gmail.com>
2024-05-07 17:37:07 +0530

    New Calc

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,12 @@ function App() {
   }
 
   const handleResult = () =>{
-    setResult(eval(data));
+    if(data === ''){
+      setResult('Error');
+    }
+    else{
+      setResult(eval(data));
+    }
   }
 
   const handleReset=()=>{
@@ -26,32 +31,32 @@ function App() {
       <h3>
         React Calculator
       </h3>
-      <input value={data} className='display'></input>
+      <input value={data} className='display' type='text' readOnly></input>
       {result}
       <div className='container'>
         <div className='row'>
-          <button onClick={handleChange} value='7' className='button'>7</button>
-          <button onClick={handleChange} value='8' className='button'>8</button>
-          <button onClick={handleChange} value='9' className='button'>9</button>
-          <button onClick={handleChange} value='+' className='button'>+</button>
+          <input type='button' onClick={handleChange} value='7' className='button'/>
+          <input type='button' onClick={handleChange} value='8' className='button'/>
+          <input type='button' onClick={handleChange} value='9' className='button'/>
+          <input type='button' onClick={handleChange} value='+' className='button'/>
         </div>
         <div className='row'>
-          <button onClick={handleChange} value='4' className='button'>4</button>
-          <button onClick={handleChange} value='5' className='button'>5</button>
-          <button onClick={handleChange} value='6' className='button'>6</button>
-          <button onClick={handleChange} value='-' className='button'>-</button>
+          <input type='button' onClick={handleChange} value='4' className='button'/>
+          <input type='button' onClick={handleChange} value='5' className='button'/>
+          <input type='button' onClick={handleChange} value='6' className='button'/>
+          <input type='button' onClick={handleChange} value='-' className='button'/>
         </div>
         <div className='row'>
-          <button onClick={handleChange} value='1' className='button'>1</button>
-          <button onClick={handleChange} value='2' className='button'>2</button>
-          <button onClick={handleChange} value='3' className='button'>3</button>
-          <button onClick={handleChange} value='*' className='button'>*</button>  
+          <input type='button' onClick={handleChange} value='1' className='button'/>
+          <input type='button' onClick={handleChange} value='2' className='button'/>
+          <input type='button' onClick={handleChange} value='3' className='button'/>
+          <input type='button' onClick={handleChange} value='*' className='button'/>  
         </div>
         <div className='row'>
-          <button onClick={handleReset} value='C' className='button'>C</button>
-          <button onClick={handleChange} value='0' className='button'>0</button>
-          <button onClick={handleResult} value='=' className='button'>=</button>
-          <button onClick={handleChange} value='/' className='button'>/</button>
+          <input type='button' onClick={handleReset} value='C' className='button'/>
+          <input type='button' onClick={handleChange} value='0' className='button'/>
+          <input type='button' onClick={handleResult} value='=' className='button'/>
+          <input type='button' onClick={handleChange} value='/' className='button'/>
         </div> 
       </div>
     </div>
